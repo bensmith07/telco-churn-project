85c882b574c35c292acc2c34b1c89004aa13352c
Ben Smith <ben.f.smith07@gmail.com>
2022-03-10 16:07:23 -0600

    initial commit of acquire, explore, prepare modules

--- acquire.py
+++ acquire.py
@@ -2,74 +2,23 @@ import os
 import pandas as pd
 from env import get_db_url, user, password, host
 
-# 1. Make a function named get_titanic_data that returns the titanic 
-# data from the codeup data science database as a pandas data frame. 
-# Obtain your data from the Codeup Data Science Database. 
-
-def get_titanic_data():
-    
-    filename = 'titanic.csv'
-    
-    if os.path.exists(filename):
-        print('Reading from local CSV...')
-        return pd.read_csv(filename)
-    
-    url = get_db_url('titanic_db')
-    sql = '''
-    SELECT * FROM passengers
-    '''
-    
-    print('No local file exists\nReading from SQL database...')
-    df = pd.read_sql(sql, url)
-
-    print('Saving to local CSV... ')
-    df.to_csv(filename, index=False)
-    
-    return df
-
-# 2. Make a function named get_iris_data that returns the data from the 
-# iris_db on the codeup data science database as a pandas data frame. 
-# The returned data frame should include the actual name of the species 
-# in addition to the species_ids. Obtain your data from the Codeup Data 
-# Science Database.
-def get_iris_data():
-    
-    filename = 'iris.csv'
-    
-    if os.path.exists(filename):
-        print('Reading from local CSV...')
-        return pd.read_csv(filename)
-        
-    url = get_db_url('iris_db')
-    sql = '''
-    SELECT *
-      FROM species
-      JOIN measurements USING(species_id);
-    '''
-    
-    print('No local file exists\nReading from SQL database...')
-    df = pd.read_sql(sql, url)
-    
-    print('Saving to local CSV...')
-    df.to_csv(filename, index=False)
-    
-    return df
-
-# 3. Make a function named get_telco_data that returns the data from the 
-# telco_churn database in SQL. In your SQL, be sure to join all 4 tables 
-# together, so that the resulting dataframe contains all the contract, 
-# payment, and internet service options. Obtain your data from the 
-# Codeup Data Science Database.
-
+# define a function for obtaining Telco Customer Data
 def get_telco_data():
     
     filename = 'telco_churn.csv'
     
+    # check for existing csv file in local directory
+    # if it exists, return it as a datframe
     if os.path.exists(filename):
         print('Reading from local CSV...')
         return pd.read_csv(filename)
     
+    # if no local directory exists, query the codeup SQL database 
+    
+    # utilize function defined in env.py to define the url
     url = get_db_url('telco_churn')
+    
+    # join the customer, contract_types, internet_service_types, and payment_types tables
     sql = '''
     SELECT * 
       FROM customers
@@ -78,17 +27,13 @@ def get_telco_data():
         JOIN payment_types USING(payment_type_id)
     '''
     
+    # return  the results of the query as a dataframe
     print('No local file exists\nReading from SQL database...')
     df = pd.read_sql(sql, url)
     
+    # save the dataframe to the local directory as a CSV for future ease of access
     print('Saving to local CSV...')
     df.to_csv(filename, index=False)
     
+    # return the dataframe
     return df
-
-# 4. Once you've got your get_titanic_data, get_iris_data, and get_telco_data 
-# functions written, now it's time to add caching to them. To do this, edit the 
-# beginning of the function to check for the local filename of telco.csv, titanic.csv, 
-# or iris.csv. If they exist, use the .csv file. If the file doesn't exist, then 
-# produce the SQL and pandas necessary to create a dataframe, then write the 
-# dataframe to a .csv file with the appropriate name.
--- explore.py
+++ explore.py
@@ -1,42 +1,229 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy.stats as stats
 
-def display_uniques(df, n_unique_limit=10):
-    '''
-    This function takes in a dataframe and displays the unique values for each column in the form of a pandas 
-    dataframe. Intended to display unique values for categorical variables. 
-    
-    n_unique_limit - default 10 - establishes the max number of unique valuescontained in the columns we 
-    want to display.
-    '''
-    # create empty df
-    newdf = pd.DataFrame()
-    
-    # for each column in the df
-    for col in df.columns:
-        # create a column in the new df that contains only the unique values from that column in the original df
-        newdf[col] = pd.Series(df[col].unique())
-    
-    # drop all columns that have a number of unique values that is greater than our established limit
-    newdf = newdf.drop(columns=newdf.columns[newdf.count() > n_unique_limit])
-    
-    # truncate the dataframe to the appropriate number of rows
-    newdf = newdf.head(newdf.count().max())
-    
-    # fill nulls with empty strings for a cleaner display
-    newdf = newdf.fillna('')
-    
-    return newdf
+#### explore univariate categorical
+def univar_categorical(train):
 
-def display_uniques_1(df, limit=10):
-    for col in df.columns[df.nunique() < limit]:
-        print(f'Column: {col}\nUnique Values: {df[col].unique()}')
-        print('----------')
+    # define categorical columns
+    categorical_columns = list(train.dtypes[train.dtypes == 'object'].index)
 
-def display_uniques_2(df, limit=10):
-    for col in df.columns:
-        print(f'Column: {col}')
-        if df[col].nunique() < 10:
-            print(f'Unique Values: {df[col].unique()}')
-        else: 
-            print(f'Range of Values: [{df[col].min()} - {df[col].max()}]')
-        print('-----------------------')
+    # for each of those columns
+    for col in categorical_columns:
+        
+        # display a frequency table
+        print(pd.DataFrame(train[col].value_counts())
+            .rename(columns={col: f'{col}_counts'}))
+        
+        # display a bar plot of those frequencies
+        sns.countplot(data=train,
+                    x=col)
+        plt.title(f'{col}_counts')
+        plt.show()
+
+#### explore univariate quantititave
+def univar_quantitative(train):
+
+    # define quantitative columns
+    quantitative_columns = ['monthly_charges', 'tenure_months']
+
+    # for each of those columns:
+    for col in quantitative_columns:
+            
+            # display a histogram of that column's distribution
+            sns.histplot(train[col], stat='proportion')
+            plt.show()
+            
+            # display a boxplot of that column's distribution
+            sns.boxplot(train[col])
+            plt.show()
+            
+            # display the summary statistics of the distribution
+            print(pd.DataFrame(train[col].describe()))
+
+
+#### explore bivariate quantitative
+def bivar_quantitative(train, target):
+
+    # define quantitative columns
+    quantitative_columns = ['monthly_charges', 'tenure_months']
+
+    # for each of the quantitative columns
+    for col in quantitative_columns:
+        # display a barplot of that variable for customers in each category of the target variable
+        sns.barplot(data=train,
+                    x=target,
+                    y=col)
+        # add a horizontal line representing the overall mean value for that variable
+        plt.axhline(train[col].mean(), 
+                    ls='--', 
+                    color='black')
+        plt.title(col, fontsize=14)
+        plt.show()
+
+#### explore multivariate 1
+
+def multivar_1(train, target):
+
+    # define quantitative columns
+    quantitative_columns = ['monthly_charges', 'tenure_months']
+
+    # create and display a pairplot comparing the distributions of each quantitative variable
+        # in each category of the target variable
+    g = sns.pairplot(train[quantitative_columns + [target]], hue=target)
+    plt.show()
+
+
+### explore correlations
+
+def multivar_absolute_correlations(train):
+
+    # create dataframe of correlation values for target variable
+    train_correlations = pd.DataFrame(train.corr().enc_churn.sort_values())
+    # use the absolute values of the correlations in a new dataframe
+    train_corr_abs = pd.DataFrame(train_correlations.apply(lambda corr: abs(corr).sort_values(ascending=False)))
+
+    return train_corr_abs
+
+
+#### chi2_test
+
+# defining a function to encapsulate repeated code for chi2 tests:
+
+def chi2_test(data_for_category1, data_for_category2, alpha=.05):
+    
+    # create dataframe of observed values
+    observed = pd.crosstab(data_for_category1, data_for_category2)
+    
+    # conduct test using scipy.stats.chi2_contingency() test
+    chi2, p, degf, expected = stats.chi2_contingency(observed)
+    
+    # round the expected values
+    expected = expected.round(1)
+    
+    # output
+    print('Observed\n')
+    print(observed.values)
+    print('---\nExpected\n')
+    print(expected)
+    print('---\n')
+    print(f'chi^2 = {chi2:.4f}')
+    print(f'p     = {p:.4f}')
+    
+    # evaluate the hypothesis against the established alpha value
+    if p < alpha:
+        print('\nReject H0')
+    else: 
+        print('\nFail to Reject H0')
+
+
+#### when are customers most likely to churn
+
+def when_customers_churn(train):
+
+    for col in ['tenure_months', 'tenure_quarters', 'tenure_years']:
+        print('=' * 50)
+        print(col.upper())
+
+        # display a histogram of tenure for customers who have churned
+        sns.histplot(train[train.churn == 'Yes'][col], stat='proportion')
+        plt.show()
+
+        # display a boxplot of months of tenure for customers who have churned
+        sns.boxplot(train[train.churn == 'Yes'][col])
+        plt.show()
+
+        # display summary statistics for months of tenure for customers who have churned
+        print(pd.DataFrame(train[train.churn == 'Yes'][col].describe()))
+        
+        # display the most common value
+        print('mode:\t\t', train[train.churn == 'Yes'][col].mode()[0])
+
+#### when are customers most likely to churn - by contract type
+
+def when_customers_churn_by_contract_type(train):
+
+    # separate the data by contract_type 
+    churned_1yr = train[(train.contract_type == 'One year') & (train.churn == 'Yes')]
+    churned_2yr = train[(train.contract_type == 'Two year') & (train.churn == 'Yes')]
+    churned_monthly = train[(train.contract_type == 'Month-to-month') & (train.churn == 'Yes')]
+
+    # for each category of contract type
+    for group in [churned_1yr, churned_2yr, churned_monthly]:
+        print('=' * 50)
+        print(group.contract_type.mode()[0])
+        
+        # display a histogram of months of tenure for that group
+        sns.histplot(group.tenure_months, stat='proportion')
+        plt.show()
+        
+        # display a boxplot of months of tenure for that group
+        sns.boxplot(group.tenure_months)
+        plt.show()
+        
+        # display summary statistics for moths of tenure for that group
+        print(pd.DataFrame(group.tenure_months.describe()))
+        
+        # display the most common value
+        print('mode:\t\t', group.tenure_months.mode()[0])
+
+
+
+#### display churn by service type
+
+def churn_by_int_service_type(train):
+
+    churn_rates = (pd.DataFrame(train.groupby(by='internet_service_type').mean().enc_churn)
+                .reset_index()
+                .sort_values(by='enc_churn'))
+    sns.barplot(data = churn_rates,
+                x = 'internet_service_type', 
+                y = 'enc_churn')
+    plt.axhline(train.enc_churn.mean(), 
+                    ls='--', 
+                    color='black')
+    plt.show()
+
+#### hypothesis test 1: MANN WHITNEY U TEST FOR MONTHLY CHARGES
+
+def hypothesis_test_monthly_charges(train):
+
+    # get the data
+    train_churned = train[train.churn == 'Yes']
+    train_not_churned = train[train.churn == 'No']
+
+    # conduct the test and display results
+
+    # Mann Whitney U test
+
+    # establish hypothesis and alpha level
+    H0 = f'mean monthly_charges for customers who have churned <= mean monthly charges for those who have not churned'
+    H1 = f'mean monthly_charges for customers who have churned > mean monthly charges for those who have not churned'
+    alpha = .05
+
+    # conduct the test
+    u, p = stats.mannwhitneyu(train_churned.monthly_charges, train_not_churned.monthly_charges, alternative='greater')
+
+    # display test info and results
+    line_break = ('\n' + '=' * 102 + '\n')
+    print(f'MANN-WHITNEY U TEST FOR: MONTHLY_CHARGES')
+    print()
+    print(f'H0: {H0}')
+    print(f'H1: {H1}')
+    print()
+    print(f'u = {u}')
+    print(f'p = {p.round(4)}')
+    print()
+    # if p < alpha:
+    #     print('RESULT: Reject H0\n')
+    #     print(f'Since p < alpha:\n')
+    #     print(f'we reject the null hypothesis that:\n\n\t{H0}\n')
+    #     print(f'and we proceed under the assumption that:\n\n\t{H1}')
+    # elif p > alpha:
+    #     print('RESULT: Fail to Reject H0\n')
+    #     print(f'Since p > alpha:\n')
+    #     print(f'we fail to reject the null hypothesis\n')
+    #     print(f'and we proceed under the assumption that:\n\n\t{H0}')
+    # print(line_break)
